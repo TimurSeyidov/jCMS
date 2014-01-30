3ee0ef65299b93bc3735514dd4cf81eec554b84a
Home Computer <tim@ethics.su>
2014-01-31 02:18:42 +0600

    Определена структура приложения. реализация модуля "Статические страницы"

--- www/index.php
+++ www/index.php
@@ -1,13 +1,10 @@
 <?php
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-require_once($yii);
-Yii::createWebApplication($config)->run();
+    $frameworkDirectory = dirname(__FILE__)."/../framework/yii.php";
+    $configurationApplicationFile = dirname(__FILE__)."/protected/config/main.php";
+    $extendFunctionFile = dirname(__FILE__)."/protected/extend/Functions.php";
+    defined("YII_DEBUG") or define("YII_DEBUG", true);
+    defined("YII_TRACE_LEVEL") or define("YII_TRACE_LEVEL",3);
+    require_once($frameworkDirectory);
+    $Application = Yii::createWebApplication($configurationApplicationFile);
+    require_once($extendFunctionFile);
+    $Application->run();
--- www/protected/config/main.php
+++ www/protected/config/main.php
@@ -1,71 +1,59 @@
 <?php
-    return array(
-        "basePath" => dirname(__FILE__).DIRECTORY_SEPARATOR."..",
-        "preload" => array("log"),
-        "import" => array(
-            "application.api.*"
-        ),
-        "theme" => "Original",
-        "defaultController" => "home/main/index",
-        "modules" => array(
-            "home"
-            // uncomment the following to enable the Gii tool
-            /*
-            "gii" => array(
-            "class" => "system.gii.GiiModule",
-            "password" => "Enter Your Password Here",
-            // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            "ipFilters" => array("127.0.0.1","::1"),
-            ),
-            */
-        ),
+return array(
+    "basePath" => dirname(__FILE__).DIRECTORY_SEPARATOR."..",
+    "preload" => array("log"),
+    "import" => array(
+        "application.api.*"
+    ),
+    "theme" => "default",
+    "defaultController" => "frontend/home/index",
+    "modules" => array(
+        "backend",
+        "frontend"
+    ),
 
-        // application components
-        "components" => array(
-            "user" => array(
-                // enable cookie-based authentication
-                "allowAutoLogin" => true,
-            ),
-            // uncomment the following to enable URLs in path-format
-            "urlManager" => array(
-                "showScriptName" => false,
-                "urlFormat" => "path",
-                "rules" => array(
-                    "<module:\w+>/main/<action:\w+>" => "<module>/<action>"
-                ),
-            ),
-            "db" => array(
-                "connectionString"  =>  "sqlite:".dirname(__FILE__)."/../data/testdrive.db",
-            ),
-            // uncomment the following to use a MySQL database
-            /*
-            "db" => array(
-            "connectionString"  =>  "mysql:host=localhost;dbname=testdrive",
-            "emulatePrepare"  =>  true,
-            "username"  =>  "root",
-            "password"  =>  "",
-            "charset"  =>  "utf8",
-            ),
-            */
-            "errorHandler" => array(
-                //"errorAction" => "site/error",
-            ),
-            "log" => array(
-                "class" => "CLogRouter",
-                "routes" => array(
-                    array(
-                        "class" => "CFileLogRoute",
-                        "levels" => "error, warning",
-                    ),
-                    // uncomment the following to show log messages on web pages
-                    /*
-                    array(
-                    "class" => "CWebLogRoute",
-                    ),
-                    */
-                ),
+    // application components
+    "components" => array(
+        "user" => array(
+            // enable cookie-based authentication
+            "allowAutoLogin" => true,
+        ),
+        // uncomment the following to enable URLs in path-format
+        "urlManager" => array(
+            "showScriptName" => false,
+            "urlFormat" => "path",
+            "rules" => array(
+                "<url:\w+>.html" => "frontend/staticpage/index",
+                //"frontend/<controller:\w+>/<action:\w+>" => "<controller>/<action>"
             ),
         ),
-        "params" => array(
+        "db" => array(
+            "connectionString" => "mysql:host=localhost;dbname=jcms",
+            "emulatePrepare" => true,
+            "username" => "root",
+            "password" => "36643545",
+            "charset" => "utf8",
+            "tablePrefix" => "jcms_"
         ),
-    );
+        "errorHandler" => array(
+            //"errorAction" => "site/error",
+        ),
+        "log" => array(
+            "class" => "CLogRouter",
+            "routes" => array(
+                array(
+                    "class" => "CFileLogRoute",
+                    "levels" => "error, warning",
+                ),
+                // uncomment the following to show log messages on web pages
+                /*
+                array(
+                "class" => "CWebLogRoute",
+                ),
+                */
+            ),
+        ),
+    ),
+    "params" => array(
+    ),
+);
