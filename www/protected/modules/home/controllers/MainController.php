b74a93b8ac5bd7459af3f338de050ff969215814
Home Computer <tim@ethics.su>
2014-01-30 21:40:05 +0600

    Revert "Revert "Revert "Определение базовой структуры"""
    This reverts commit 24cdc13bdb932facb732b0808ed69e4943aa68eb.

--- www/protected/modules/home/controllers/MainController.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class MainController extends CController{
-        public function actionIndex(){
-            $this->render("index", array("a" => "sdf"));
-        }
-        
-    }
-?>
